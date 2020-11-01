f596c4093366a8a6ad8b623c6e2e490a2094b73a
Moras-del <morekszymon@wp.pl>
2020-11-01 16:51:50 +0100

    test-commit

--- src/main/java/pl/ug/kuznia/todoappapi/TodoAppApiApplication.java
+++ src/main/java/pl/ug/kuznia/todoappapi/TodoAppApiApplication.java
@@ -2,12 +2,19 @@ package pl.ug.kuznia.todoappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodoAppApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoAppApiApplication.class, args);
     }
 
+    @GetMapping
+    String hello(){
+        return "hello";
+    }
 }
